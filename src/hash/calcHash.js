ebf0a1e98dfd75ad2d688f12c23a8e4652029752
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-04-11 00:43:57 +0500

    Hash task is done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,11 @@
+import { createHash } from "node:crypto";
+import { readFileSync } from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = "./src/hash/files/fileToCalculateHashFor.txt";
+  const fileContent = readFileSync(filePath);
+  const hex = createHash("sha256").update(fileContent).digest("hex");
+  console.log(hex);
 };
 
 await calculateHash();
