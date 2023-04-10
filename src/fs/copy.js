30837fd299e3f187154d88d5651aefee1dba3d8c
Hasanboy Marufjonov <therealhasanboy@gmail.com>
2023-04-11 00:01:20 +0500

    create.js and copy.js done

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,34 @@
-const copy = async () => {
-    // Write your code here 
+import fs from "fs";
+import path from "path";
+
+const copy = async (src, dest) => {
+  const srcExists = fs.existsSync(src);
+  const destExists = fs.existsSync(dest);
+
+  if (!srcExists) {
+    throw new Error("FS operation failed");
+  }
+
+  if (destExists) {
+    throw new Error("FS operation failed");
+  }
+
+  fs.mkdirSync(dest);
+
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copy(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+
+  console.log(`Directory "${src}" copied to "${dest}" successfully.`);
 };
 
-await copy();
+await copy("./src/fs/files", "./src/fs/files_copy");
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import fs, { existsSync } from "fs";
+
 const create = async () => {
-    // Write your code here 
+  if (!fs.existsSync("./src/fs/files/fresh.txt")) {
+    fs.writeFile("./src/fs/files/fresh.txt", "I am fresh and young", (err) => {
+      if (err) throw err;
+    });
+  } else {
+    throw Error("FS operation failed");
+  }
 };
 
 await create();
